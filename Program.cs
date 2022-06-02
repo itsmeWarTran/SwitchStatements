b2bddcd3f7312e63ca06fd3e257674d1bbcb408a
Warren Tran <warrentran2015@hotmail.com>
2022-06-02 12:51:55 -0400

    descriptive message

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite school subject");
+            String subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                
+                
+                case "math":
+                Console.WriteLine("Your favorite subject is math");
+                break;
+
+                case "science":
+                Console.WriteLine("Your favorite subject is science");
+                break;
+
+                case "english":
+                Console.WriteLine("Your favorite subject is english");
+                break;
+
+                case "history":
+                Console.WriteLine("Your favorite subject is history");
+                break;
+
+                case "spanish":
+                Console.WriteLine("Your favorite subject is spanish");
+                break;
+                
+                default:
+                Console.WriteLine($"Your favorite subject is {subject}");
+                break;
+            }
+        }
+    }
+}
